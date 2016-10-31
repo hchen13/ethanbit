7be0254762980ebb88991102886c2b67380e3290
EthanDivestar <divestar@divestardeMacBook-Pro-3.local>
2016-10-31 17:24:19 +0800

    production config

--- ethanbit/wsgi.py
+++ ethanbit/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ethanbit.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ethanbit.settings.prod_aws_seoul")
 
 application = get_wsgi_application()
