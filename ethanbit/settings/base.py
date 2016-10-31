cbc582d0bc46ad151504a961db9521357f7c65df
EthanDivestar <divestar@divestardeMacBook-Pro-3.local>
2016-10-31 15:22:01 +0800

    add deploy static root

--- ethanbit/settings/base.py
+++ ethanbit/settings/base.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_deploy')
 
 # Media files (user-generated files)
 
